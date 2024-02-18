1419e648413d70b18b79b289235915198a16c3c4
Caio Gomes <ocaioluis@gmail.com>
2024-02-18 22:48:56 +0000

    wip: add rusb as depedency for rust
    Signed-off-by: Caio Gomes <ocaioluis@gmail.com>

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -8,6 +8,18 @@ fn greet(name: &str) -> String {
 }
 
 fn main() {
+    for device in rusb::devices().unwrap().iter() {
+        let device_desc = device.device_descriptor().unwrap();
+
+        println!(
+            "Bus {:03} Device {:03} ID {:04x}:{:04x}",
+            device.bus_number(),
+            device.address(),
+            device_desc.vendor_id(),
+            device_desc.product_id()
+        );
+    }
+
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
